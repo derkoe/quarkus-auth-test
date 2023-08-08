f4b40b786999945cc74c36389ab4b854e486ea57
Christian Köberl <christian.koeberl@porscheinformatik.com>
2023-08-08 22:40:49 +0200

    Try to fix test
    Test does not run because basic auth is not active when running unit tests.

--- src/test/java/dev/derkoe/AuthenticationTest.java
+++ src/test/java/dev/derkoe/AuthenticationTest.java
@@ -17,20 +17,9 @@ class AuthenticationTest {
     @TestHTTPResource("hello")
     URL frontendUrl;
 
-    @TestHTTPEndpoint(FrontendApiResource.class)
+    @TestHTTPEndpoint(BackendApiResource.class)
     @TestHTTPResource("hello")
     URL backendUrl;
-
-    @Test
-    void testBackendWithAuth() {
-        given()
-                .redirects().follow(false)
-                .auth().basic("admin", "password")
-                .when().get(backendUrl)
-                .then()
-                .statusCode(200)
-                .body(containsString("Backend API"));
-    }
 
     @Test
     void testFrontendEndpoint() {
@@ -50,5 +39,16 @@ class AuthenticationTest {
                 .then()
                 .statusCode(401)
                 .header("www-authenticate", containsString("basic"));
+    }
+
+    @Test
+    void testBackendWithAuth() {
+        given()
+                .redirects().follow(false)
+                .auth().basic("admin", "password")
+                .when().get(backendUrl)
+                .then()
+                .statusCode(200)
+                .body(containsString("Backend API"));
     }
 }
